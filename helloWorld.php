65cbca7e17ba1b03faf892e749db9984883d3de5
Tan Nguyen <tannguyen1220@gmail.com>
2022-04-30 18:47:40 +0700

    first commit

---
+++ helloWorld.php
@@ -0,0 +1,26 @@
+<?php  
+    echo"hello world from me a echo php line of code, the best waifu is ";
+    print_r(['emilia','christina','mary']);
+    $best_waifu = 'emilia';
+    echo 'best waifu is: '.$best_waifu;
+
+
+
+
+?>
+
+<html lang="en">
+
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Document</title>
+    </head>
+
+    <body>
+        <h1> <?php echo 'hello again'?> </h1>
+
+    </body>
+
+</html>
