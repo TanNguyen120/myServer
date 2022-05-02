393b52a67cfe3c1ff8da6a1e0e3319ebbd3b08c1
Tan Nguyen <tannguyen1220@gmail.com>
2022-05-02 10:10:20 +0700

    Get global variable

--- helloWorld.php
+++ helloWorld.php
@@ -7,5 +7,22 @@
             echo $i . " x " . $n . " = " . $result . "<br>"; 
         }
     }
-    cuuChuong(13);
+    cuuChuong($_GET['number']);
 ?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Document</title>
+    </head>
+
+    <body>
+        <a href="http://localhost/myServer/helloWorld.php?number=4">Xem bảng cửu chương 4 tại đây</a>
+    </body>
+
+</html>
