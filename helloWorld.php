478f22af4dc6f4d2748da76d7ed303e8e90dda66
Tan Nguyen <tannguyen1220@gmail.com>
2022-05-02 08:56:44 +0700

    fisrt function

--- helloWorld.php
+++ helloWorld.php
@@ -1,26 +1,11 @@
-<?php  
-    echo"hello world from me a echo php line of code, the best waifu is ";
-    print_r(['emilia','christina','mary']);
-    $best_waifu = 'emilia';
-    echo 'best waifu is: '.$best_waifu;
-
-
-
-
+<?php 
+    function cuuChuong($n){
+        echo "<h3> bảng cửu chương ${n} </h3>";
+        for($i = 0; $i < 10;$i++)
+        {
+            $result = $i * $n;
+            echo $i . " x " . $n . " = " . $result . "<br>"; 
+        }
+    }
+    cuuChuong(13);
 ?>
-
-<html lang="en">
-
-    <head>
-        <meta charset="UTF-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Document</title>
-    </head>
-
-    <body>
-        <h1> <?php echo 'hello again'?> </h1>
-
-    </body>
-
-</html>
